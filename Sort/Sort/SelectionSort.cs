807d2cf872688ded8f1a023568af6810f5a3cf3a
Toàn Trần <32505254+ttoan12@users.noreply.github.com>
2017-10-07 14:56:28 +0700

    Selection Sort

---
+++ Sort/Sort/SelectionSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sort
+{
+    class SelectionSort
+    {
+        // Using IComparable
+        public static void Sort<T>(T[] list)
+            where T : IComparable<T>
+        {
+            int length = list.Length - 1;
+            for (int i = 0; i < length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j <= length; j++)
+                {
+                    if (list[j].CompareTo(list[min]) == -1) min = j;
+                }
+                if (min != i)
+                    Swap(ref list[i], ref list[min]);
+            }
+        }
+
+        // Using Comparer
+        public static void Sort<T>(Comparer<T> cmp, T[] list)
+        {
+            int length = list.Length - 1;
+            for (int i = 0; i < length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j <= length; j++)
+                {
+                    if (cmp.Compare(list[j], list[min]) == -1) min = j;
+                }
+                if (min != i)
+                    Swap(ref list[i], ref list[min]);
+            }
+        }
+
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
