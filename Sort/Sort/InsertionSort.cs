917917c03b0f3d86d09db65bf0df801b0fdd706b
Toàn Trần <32505254+ttoan12@users.noreply.github.com>
2017-10-07 14:56:03 +0700

    Insertion Sort

---
+++ Sort/Sort/InsertionSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sort
+{
+    class InsertionSort
+    {
+        // Using IComparable
+        public static void Sort<T>(T[] list)
+            where T : IComparable<T>
+        {
+            for (int sorted = 0; sorted < list.Length - 1; sorted++)
+            {
+                int i = sorted;
+                while (list[i].CompareTo(list[i + 1]) == 1)
+                {
+                    Swap(ref list[i], ref list[i + 1]);
+                    if (i > 0) i--;
+                }
+            }
+        }
+
+        // Using Comparer
+        public static void Sort<T>(Comparer<T> cmp, T[] list)
+        {
+            for (int sorted = 0; sorted < list.Length - 1; sorted++)
+            {
+                int i = sorted;
+                while (cmp.Compare(list[i], list[i + 1]) == 1)
+                {
+                    Swap(ref list[i], ref list[i + 1]);
+                    if (i > 0) i--;
+                }
+            }
+        }
+
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+    }
+}
