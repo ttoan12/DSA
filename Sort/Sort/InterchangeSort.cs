52bcf5d87141ce322b6633eb0cb0610cbc4aa4f4
Toàn Trần <32505254+ttoan12@users.noreply.github.com>
2017-10-07 14:54:50 +0700

    Interchange Sort

---
+++ Sort/Sort/InterchangeSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sort
+{
+    class InterchangeSort
+    {
+        // Using IComparable
+        public static void Sort<T>(T[] list)
+            where T : IComparable<T>
+        {
+            int length = list.Length;
+            for (int i = 0; i < length - 1; i++)
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (list[i].CompareTo(list[j]) == 1)
+                        Swap(ref list[i], ref list[j]);
+                }
+        }
+
+        // Using Comparer
+        public static void Sort<T>(Comparer<T> cmp, T[] list)
+        {
+            int length = list.Length;
+            for (int i = 0; i < length - 1; i++)
+                for (int j = i + 1; j < length; j++)
+                {
+                    if (cmp.Compare(list[i], list[j]) == 1)
+                        Swap(ref list[i], ref list[j]);
+                }
+        }
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
